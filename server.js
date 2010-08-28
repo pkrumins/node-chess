aebe96cd1765ae2be77b766cfc99011ca3f30ecc
James Halliday <substack@gmail.com>
2010-08-27 17:43:44 -0800

    crazy require/include business

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
+#!/usr/bin/env node
+var fs = require('fs');
 var connect = require('connect');
 var DNode = require('dnode');
+var Service = require('./lib/service');
 
 var port = process.argv[2] || 80;
 
-connect.createServer(
+var html = { index : fs.readFileSync(__dirname + '/static/index.html') };
+var js = { dnode : require('dnode/web').source() };
+
+var server = connect.createServer(
     connect.staticProvider(__dirname + '/static'),
+    connect.staticProvider(__dirname + '/lib'),
     function (req, res) {
-        res.writeHead(200, { 'Content-Type' : 'text/html' });
-        res.end('meow');
+        if (req.url == '/scripts/dnode.js') {
+            res.writeHead(200, { 'Content-Type' : 'text/javascript' });
+            res.end(js.dnode);
+        }
+        else {
+            res.writeHead(200, { 'Content-Type' : 'text/html' });
+            res.end(html.index);
+        }
     }
 ).listen(port, '0.0.0.0');
+
+DNode(Service).listen({
+    protocol : 'socket.io',
+    server : server,
+    transports : 'websocket xhr-multipart xhr-polling htmlfile'
+        .split(/\s+/),
+});
