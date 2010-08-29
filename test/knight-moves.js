b0f9f3bf01c02c9b7b8f0164ff72a8cc14c67603
Peteris Krumins <peteris.krumins@gmail.com>
2010-08-29 04:21:29 +0300

    add no knight move test

--- test/knight-moves.js
+++ test/knight-moves.js
@@ -2,8 +2,6 @@ var Board = require('../lib/game/board');
 var MoveGenerator = require('../lib/game/movegenerator');
 
 var b = new Board;
-var b2 = new Board;
-
 b.move(1,0,4,4);
 
 function newState() {
@@ -32,8 +30,17 @@ module.exports = {
         assert.equal(ok, 8);
     },
     'some-knight-moves' : function (assert) {
+        var b2 = new Board;
         var moves = MoveGenerator(b2, newState(), b2.at(1,0), true);
         assert.equal(moves.length, 2);
+    },
+    'no-knight-moves' : function (assert) {
+        var b3 = new Board;
+        b3.move(1,0,4,2);
+        b3.move(0,1,2,3);       
+        b3.move(1,1,3,4);       
+        b3.move(3,1,5,4);       
+        b3.move(4,1,6,3);       
     }
 };
 
