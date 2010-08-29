2a7254fce5fb8145c618211549514227c502f54b
Peteris Krumins <peteris.krumins@gmail.com>
2010-08-29 03:28:07 +0300

    pass state and true for king check to MoveGenerator

--- test/pawn-moves.js
+++ test/pawn-moves.js
@@ -11,29 +11,35 @@ b2.move(0,6,0,4); // b7-b5
 b3.move(0,1,0,3);
 b3.move(1,6,1,4);
 
+function newState() {
+    return {
+        moves : []
+    };
+}
+
 module.exports = {
     'initial-pawn-moves-white': function (assert) {
-        assert.equal(MoveGenerator(null,b.at(0,1)).length, 2);
-        assert.equal(MoveGenerator(null,b.at(1,1)).length, 2);
-        assert.equal(MoveGenerator(null,b.at(2,1)).length, 2);
-        assert.equal(MoveGenerator(null,b.at(3,1)).length, 2);
-        assert.equal(MoveGenerator(null,b.at(4,1)).length, 2);
-        assert.equal(MoveGenerator(null,b.at(5,1)).length, 2);
-        assert.equal(MoveGenerator(null,b.at(6,1)).length, 2);
-        assert.equal(MoveGenerator(null,b.at(7,1)).length, 2);
+        assert.equal(MoveGenerator(b,newState(),b.at(0,1),true).length, 2);
+        assert.equal(MoveGenerator(b,newState(),b.at(1,1),true).length, 2);
+        assert.equal(MoveGenerator(b,newState(),b.at(2,1),true).length, 2);
+        assert.equal(MoveGenerator(b,newState(),b.at(3,1),true).length, 2);
+        assert.equal(MoveGenerator(b,newState(),b.at(4,1),true).length, 2);
+        assert.equal(MoveGenerator(b,newState(),b.at(5,1),true).length, 2);
+        assert.equal(MoveGenerator(b,newState(),b.at(6,1),true).length, 2);
+        assert.equal(MoveGenerator(b,newState(),b.at(7,1),true).length, 2);
     },
     'initial-pawn-moves-black': function (assert) {
-        assert.equal(MoveGenerator(null,b.at(0,6)).length, 2);
-        assert.equal(MoveGenerator(null,b.at(1,6)).length, 2);
-        assert.equal(MoveGenerator(null,b.at(2,6)).length, 2);
-        assert.equal(MoveGenerator(null,b.at(3,6)).length, 2);
-        assert.equal(MoveGenerator(null,b.at(4,6)).length, 2);
-        assert.equal(MoveGenerator(null,b.at(5,6)).length, 2);
-        assert.equal(MoveGenerator(null,b.at(6,6)).length, 2);
-        assert.equal(MoveGenerator(null,b.at(7,6)).length, 2);
+        assert.equal(MoveGenerator(b,newState(),b.at(0,6),true).length, 2);
+        assert.equal(MoveGenerator(b,newState(),b.at(1,6),true).length, 2);
+        assert.equal(MoveGenerator(b,newState(),b.at(2,6),true).length, 2);
+        assert.equal(MoveGenerator(b,newState(),b.at(3,6),true).length, 2);
+        assert.equal(MoveGenerator(b,newState(),b.at(4,6),true).length, 2);
+        assert.equal(MoveGenerator(b,newState(),b.at(5,6),true).length, 2);
+        assert.equal(MoveGenerator(b,newState(),b.at(6,6),true).length, 2);
+        assert.equal(MoveGenerator(b,newState(),b.at(7,6),true).length, 2);
     },
     'single-initial-black-move' : function (assert) {
-        moves = MoveGenerator(null, b.at(0,6));
+        moves = MoveGenerator(b,newState(), b.at(0,6),true);
         var ok = 0;
         for (var i = 0; i < moves.length; i++) {
             var move = moves[i];
@@ -43,7 +49,7 @@ module.exports = {
         assert.equal(ok,2);
     },
     'single-initial-white-move' : function (assert) {
-        moves = MoveGenerator(null, b.at(0,1));
+        moves = MoveGenerator(b,newState(), b.at(0,1),true);
         var ok = 0;
         for (var i = 0; i < moves.length; i++) {
             var move = moves[i];
@@ -53,11 +59,11 @@ module.exports = {
         assert.equal(ok,2);
     },
     'cant-move' : function (assert) {
-        assert.equal(MoveGenerator(null,b2.at(0,3)).length, 0);
-        assert.equal(MoveGenerator(null,b2.at(0,4)).length, 0);
+        assert.equal(MoveGenerator(b2,newState(),b2.at(0,3),true).length, 0);
+        assert.equal(MoveGenerator(b2,newState(),b2.at(0,4),true).length, 0);
     },
     'capture-or-move' : function (assert) {
-        var moves = MoveGenerator(null,b3.at(0,3));
+        var moves = MoveGenerator(b3,newState(),b3.at(0,3),true);
         assert.equal(moves.length,2);
 
         var ok = 0;
