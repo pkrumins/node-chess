a37a9a2aa03bc2bd636eb43b23929ef9a18d93fc
James Halliday <substack@gmail.com>
2010-08-29 09:35:03 -0800

    take out script kludge, roll into a single /bundle.js for much faster loads

--- server.js
+++ server.js
@@ -9,16 +9,28 @@ var Service = require('./lib/service');
 var port = process.argv[2] || 80;
 
 var html = { index : fs.readFileSync(__dirname + '/static/index.html') };
-var js = { dnode : require('dnode/web').source() };
+
+// All javascript into one file for MOAR FASTAR page loads
+var js = [
+    require('dnode/web').source(),
+    ('render/vendor/jquery-min render/vendor/raphael-min'
+    + ' events game/square game/board game/movegenerator game/player game/game'
+    + ' render/thumbnail render/viewer client')
+    .split(/\s+/).map(function (filename) {
+        return fs.readFileSync(__dirname + '/lib/' + filename + '.js')
+            .toString()
+            .replace(/^(module|exports)\..*/mg, '')
+            .replace(/^var \S+\s*=\s*require\(.*/mg, '')
+        ;
+    }).join('\n')
+].join('\n');
 
 var server = connect.createServer(
     connect.staticProvider(__dirname + '/static'),
-    connect.staticProvider(__dirname + '/lib'),
     function (req, res) {
-        var m = req.url.match(/([^\/.]+)\.js/);
-        if (m && m[1] in js) {
+        if (req.url == '/bundle.js') {
             res.writeHead(200, { 'Content-Type' : 'text/javascript' });
-            res.end(js[m[1]]);
+            res.end(js);
         }
         else {
             res.writeHead(200, { 'Content-Type' : 'text/html' });
